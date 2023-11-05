354d13282832cc9b85c5fd3acb268dd35d6f9c08
Park Sejin <jinpark99@naver.com>
2023-11-05 10:19:45 +0900

    feat: init commit

---
+++ src/main/java/com/finfellows/FinfellowsApplication.java
@@ -0,0 +1,13 @@
+package com.finfellows;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinfellowsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinfellowsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/finfellows/FinfellowsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finfellows;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinfellowsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
