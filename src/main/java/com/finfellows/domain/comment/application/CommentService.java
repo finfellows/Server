1a4c2f986d2bfca2b2fddbc6f1153085ab068e8c
EunbeenDev <ros4312@naver.com>
2024-01-07 03:39:10 +0900

    [FEAT] #39 챗봇 질의응답 기능 개발
    인사말 추가, 질의응답, 조회 기능

--- src/main/java/com/finfellows/domain/comment/application/CommentService.java
+++ src/main/java/com/finfellows/domain/comment/application/CommentService.java
@@ -1,6 +1,8 @@
 package com.finfellows.domain.comment.application;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.finfellows.domain.chatgpt.domain.ChatGptMessage;
@@ -9,7 +11,10 @@ import com.finfellows.domain.chatgpt.config.ChatgptConfig;
 import com.finfellows.domain.chatgpt.dto.request.ChatgptQuestionRequest;
 import com.finfellows.domain.chatgpt.dto.request.ChatgptRequest;
 import com.finfellows.domain.chatgpt.dto.response.ChatgptResponse;
+import com.finfellows.domain.comment.domain.Comment;
 import com.finfellows.domain.comment.domain.repository.CommentRepository;
+import com.finfellows.domain.comment.dto.response.CommentResponse;
+import com.finfellows.domain.user.domain.User;
 import com.finfellows.domain.user.domain.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,6 +25,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -30,6 +36,7 @@ public class CommentService {
 
     @Autowired
     private RestTemplate restTemplate;
+
 
     @Value("${chatgpt.api-key}")
     private String apiKey;
@@ -47,7 +54,6 @@ public class CommentService {
 
     // gpt 단답
     public ChatgptResponse getResponse(HttpEntity<ChatgptRequest> chatGptRequestHttpEntity) {
-
         SimpleClientHttpRequestFactory requestFactory = new SimpleClientHttpRequestFactory();
         requestFactory.setConnectTimeout(60000);
         //답변이 길어질 경우 TimeOut Error가 발생하니 1분정도 설정해줍니다.
@@ -80,5 +86,62 @@ public class CommentService {
                         )
                 )
         );
+    }
+
+    public String getChatResponse(String question){
+        String responseFromGPT=chatGptService.getChatResponse(question);
+        return responseFromGPT;
+    }
+
+    public void saveComment(Long userId, String question, String answer) {
+        Optional<User> optionalUser = userRepository.findById(userId);
+        User user = optionalUser.orElseThrow(() -> new RuntimeException("User not found"));
+
+        question = extractPromptFromJson(question);
+        answer = answer.replaceAll("\\n", "");
+        answer = answer.replaceAll("금토리: ", "");
+        answer = answer.replaceAll("네. ", "");
+
+        Comment comment = Comment.builder()
+                .question(question)
+                .answer(answer)
+                .user(user)
+                .build();
+        commentRepository.save(comment);
+    }
+
+    // JSON에서 "prompt" 부분 추출하는 메소드
+    private String extractPromptFromJson(String json) {
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            JsonNode jsonNode = objectMapper.readTree(json);
+            if (jsonNode.has("prompt")) {
+                return jsonNode.get("prompt").asText();
+            }
+        } catch (JsonProcessingException e) {
+            System.out.print("텍스트 변환 실패");
+        }
+        return json;
+    }
+
+    public List<CommentResponse> getAllComments(Long userId) {
+        List<Comment> comments = commentRepository.findAllByUserId(userId);
+
+        Comment greet = Comment.builder()
+                .greeting("안녕! 나는 금토리야. 도움이 필요하다면 편하게 말해줘.")
+                .build();
+        commentRepository.save(greet);
+
+        return comments.stream()
+                .map(comment -> CommentResponse.builder()
+                        .commentId(comment.getCommentId())
+                        .created_at(comment.getCreatedAt())
+                        .greeting(comment.getGreeting())
+                        .question(comment.getQuestion())
+                        .answer(comment.getAnswer())
+                        .userId(comment.getUser())
+                        .build()
+                )
+                .collect(Collectors.toList());
     }
 }
--- src/main/java/com/finfellows/domain/comment/presentation/CommentController.java
+++ src/main/java/com/finfellows/domain/comment/presentation/CommentController.java
@@ -1,9 +1,46 @@
 package com.finfellows.domain.comment.presentation;
 
+import com.finfellows.domain.chatgpt.application.ChatGptService;
+import com.finfellows.domain.comment.application.CommentService;
+import com.finfellows.domain.comment.dto.response.CommentResponse;
+import com.finfellows.domain.user.application.UserService;
+import com.finfellows.global.config.security.token.CurrentUser;
+import com.finfellows.global.config.security.token.UserPrincipal;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/chatbot")
+@Tag(name="Chatbot",description = "Chatbot API")
 public class CommentController {
+    private CommentService commentService;
+    private ChatGptService chatGptService;
+    private UserService userService;
+
+
+    @Autowired
+    public CommentController(CommentService commentService, ChatGptService chatGptService, UserService userService) {
+        this.commentService = commentService;
+        this.chatGptService = chatGptService;
+        this.userService = userService;
+    }
+
+    @PostMapping("")
+    public String getChatResponse(@CurrentUser UserPrincipal userPrincipal, @RequestBody String question){
+        String answer= commentService.getChatResponse(question);
+        commentService.saveComment(userPrincipal.getId(), question, answer);
+        return answer;
+    }
+    @GetMapping("")
+    public ResponseEntity<List<CommentResponse>> getAllComments(@CurrentUser UserPrincipal userPrincipal){
+        List<CommentResponse> responseList=commentService.getAllComments(userPrincipal.getId());
+        return new ResponseEntity<>(responseList, HttpStatus.OK);
+    }
 }
