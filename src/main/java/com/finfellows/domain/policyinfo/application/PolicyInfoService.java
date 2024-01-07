d0f5dca8169fb2b5185f6f85cb7b82eee32e19ec
박세진 <jinpark99@naver.com>
2024-01-07 15:23:30 +0900

    [FEAT]: PolicyInfo 리스트 조회 API 구현

---
+++ src/main/java/com/finfellows/domain/policyinfo/application/PolicyInfoService.java
@@ -0,0 +1,13 @@
+package com.finfellows.domain.policyinfo.application;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+@Transactional(readOnly = true)
+public class PolicyInfoService {
+
+
+}
