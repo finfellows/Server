d02232fccd2b63bdf6757005a331b3f6bc0a58b7
EunbeenDev <ros4312@naver.com>
2024-01-07 14:12:16 +0900

    [FIX] #39 swagger 코드 추가

--- src/main/java/com/finfellows/domain/comment/presentation/CommentController.java
+++ src/main/java/com/finfellows/domain/comment/presentation/CommentController.java
@@ -6,6 +6,11 @@ import com.finfellows.domain.comment.dto.response.CommentResponse;
 import com.finfellows.domain.user.application.UserService;
 import com.finfellows.global.config.security.token.CurrentUser;
 import com.finfellows.global.config.security.token.UserPrincipal;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -32,12 +37,21 @@ public class CommentController {
         this.userService = userService;
     }
 
+    @Operation(summary = "챗봇 질의응답 저장", description = "챗봇 질문과 답변을 저장합니다.")
+    @ApiResponse(responseCode = "200", description = "챗봇 질문에 대한 답변 응답 성공", content = {
+            @Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = CommentResponse.class)))
+    })
     @PostMapping("")
     public String getChatResponse(@CurrentUser UserPrincipal userPrincipal, @RequestBody String question){
         String answer= commentService.getChatResponse(question);
         commentService.saveComment(userPrincipal.getId(), question, answer);
         return answer;
     }
+
+    @Operation(summary = "챗봇 대화 내용 조회", description = "챗봇 대화 전체 목록을 조회합니다.")
+    @ApiResponse(responseCode = "200", description = "챗봇 대화 목록 조회 성공", content = {
+            @Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = CommentResponse.class)))
+    })
     @GetMapping("")
     public ResponseEntity<List<CommentResponse>> getAllComments(@CurrentUser UserPrincipal userPrincipal){
         List<CommentResponse> responseList=commentService.getAllComments(userPrincipal.getId());
