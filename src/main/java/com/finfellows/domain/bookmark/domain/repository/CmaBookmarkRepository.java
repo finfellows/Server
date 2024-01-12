d8d86029f4f57884af59a579c3efe7348934c716
LeeJaeHyeok97 <hazardous10@naver.com>
2024-01-13 07:41:07 +0900

    [FIX] CMA 엔티티 명에 따른 JPQL 작성

--- src/main/java/com/finfellows/domain/bookmark/domain/repository/CmaBookmarkRepository.java
+++ src/main/java/com/finfellows/domain/bookmark/domain/repository/CmaBookmarkRepository.java
@@ -4,6 +4,8 @@ import com.finfellows.domain.bookmark.domain.CmaBookmark;
 import com.finfellows.domain.product.domain.CMA;
 import com.finfellows.domain.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -11,5 +13,6 @@ import java.util.Optional;
 public interface CmaBookmarkRepository extends JpaRepository<CmaBookmark, Long> {
     List<CmaBookmark> findAllByUser(User user);
 
-    Optional<CmaBookmark> findByUserAndCma(User user, CMA cma);
+    @Query("SELECT bm FROM CmaBookmark bm WHERE bm.user = :user AND bm.cma = :cma")
+    Optional<CmaBookmark> findByUserAndCma(@Param("user") User user, @Param("cma") CMA cma);
 }
