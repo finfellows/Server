8996924631214da63acc05c05833642fafb03850
EunbeenDev <ros4312@naver.com>
2024-01-15 01:54:29 +0900

    [FEAT] #133 S3 버킷 이미지 업로드 기능 swagger 코드 추가

--- src/main/java/com/finfellows/domain/image/presentation/ImageControlller.java
+++ src/main/java/com/finfellows/domain/image/presentation/ImageControlller.java
@@ -1,7 +1,13 @@
 package com.finfellows.domain.image.presentation;
 
+import com.finfellows.domain.comment.dto.response.CommentListResponse;
 import com.finfellows.domain.image.application.ImageService;
 import com.finfellows.domain.image.dto.response.ImageResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +21,10 @@ import java.io.IOException;
 @RequestMapping("/api/image")
 public class ImageControlller {
     private final ImageService imageService;
+    @Operation(summary = "이미지 업로드", description = "S3 버킷에 이미지를 업로드 합니다. 파라미터로 image 파일을 요청하고, url 주소로 응답합니다. ")
+    @ApiResponse(responseCode = "200", description = "이미지 업로드 및 url 변환 성공", content = {
+            @Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = CommentListResponse.class)))
+    })
     @PostMapping
     public ResponseEntity<ImageResponse> uploadImage(@RequestParam MultipartFile image) throws IOException {
         ImageResponse response = imageService.uploadImageToS3(image);
